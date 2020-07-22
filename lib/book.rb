290647306e6a74d1ed6731e0d359d9e959374681
Andy Adams <andrewdcadams@gmail.com>
2020-07-22 14:07:08 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,25 @@
+require 'pry'
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  
+  GENRES = []
 
   def initialize(title)
     @title = title
+    puts @title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    #puts @genre 
+    GENRES << @genre
+    #binding.pry
+    #puts @genre
   end
 
 end
